77b36982d90482b5fad654b2f671a0951824f952
Rafael Kampa <rafael.kampa@mps.com.br>
2022-12-08 12:01:01 -0300

    Código alterado para o branch correspondente ao curso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,32 @@
-import {AfterViewInit, Component, ContentChild, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
-import { CourseCardComponent } from './courses/course-card/course-card.component';
-import { CourseImageComponent } from './courses/course-image/course-image.component';
-import { Course } from './model/course';
-
+import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit, OnInit{
+export class AppComponent implements AfterViewInit {
 
     courses = COURSES;
 
-    //ViewChild permite instanciar apenas um componente por vez, para instanciar uma Query de componentes é necessário usar a anotação ViewChildren
-    @ViewChild('cardRef', {read: ElementRef})
-    card: CourseCardComponent;
-
-    @ViewChild('courseImage')
-    courseImage: ElementRef;
 
     @ViewChildren(CourseCardComponent, {read: ElementRef})
-    cards: QueryList<Course>;
+    cards : QueryList<ElementRef>;
 
-    //ContentChild mostra somente o os dados do ng-content que foram declarados no componente app.html (neste caso o courseImage)
-    @ContentChild(CourseImageComponent, {read: ElementRef})
-    image: ElementRef;
-
-    startDate = new Date();
 
     constructor() {
+
     }
 
-    //OnInit é carregado após o primeiro OnChanges, ou seja, é carregado após carregar todos os dados necessários
-    ngOnInit(): void {
-        console.log(this.image); 
+    ngAfterViewInit() {
+
     }
 
-    //AfterViewInit carrega antes do que o OnInit, mas as vezes pode carregar antes que todos os componentes tenham sido carregados
-    ngAfterViewInit(): void { 
-        console.log(this.cards);
-    }
-    
     onCourseSelected(course:Course) {
-        console.log(this.card)
-    }
 
-    onCoursesEdited() {
-        this.courses.push(
-            {
-                id: 11,
-                description: "New Course",
-                iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
-                longDescription: "Just a test for the query",
-                lessonsCount: 10,
-                category: 'ALL LEVELS'
-            }
-        )
     }
 
 }
